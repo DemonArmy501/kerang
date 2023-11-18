570bc789f66364b338bf6c46d647e25807065fd5
DemonArmy <136950303+DemonArmy501@users.noreply.github.com>
2023-11-19 03:37:14 +0700

    ss.php

---
+++ ss.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+   <meta name="robots" content="index,follow">
+    <meta name="language" content="Indonesia">
+    <meta name="author" content="Haxor Noname">
+	<meta name="Hacked By Haxor Noname" content="keywords" />
+	<meta name="Hacked By" content="keywords" />
+    <meta name="googlebot" content="index,follow" />
+    <meta name="rating" content="general" />
+    <meta name="geo.region" content="id_ID" />
+    <meta name="geo.country" content="id" />
+    <meta name="language" content="Id-ID" />
+    <meta name="distribution" content="global" />
+    <meta name="geo.placename" content="Indonesia" />
+  <style type='text/css'>body,a:hover{cursor:url(http://cur.cursors-4u.net/symbols/sym-1/sym46.cur),progress!important;}</style>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hacked By Haxor Noname</title>
+  <link rel="stylesheet" href="https://sin.bnn.go.id/assets/css/grenns.css">
+</head>
+<body>
+  <div class="container">
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/FHWgDichnMo?si=RwO7zgby9p-IwI0b" frameborder="0" allowfullscreen></iframe>
+    <div class="error-text">Not Found</div>
+    <div class="error-msg">Hacked By Haxor Noname</div>
+    <div class="error-info">We are watching you! ahahahaha</div><br>
+    <div class="error-info">Leviathan Perfect Hunter</div>
+    <div class="error-contact">Contact: HaxorNoname@protonmail.com</div>
+  </div>
+</body>
+</html>
